a198a3e77d5c759b7ce983740938cafeddbd3002
keito0tada <keito0tada@gmail.com>
2023-04-28 16:54:15 +0900

    temp progress

--- src/cogs/progress.py
+++ src/cogs/progress.py
@@ -2,7 +2,7 @@ import zoneinfo
 
 import discord
 from discord.ext import commands, tasks
-from typing import Dict, List
+from typing import Dict, List,Optional
 import psycopg2
 import psycopg2.extras
 import datetime
@@ -12,21 +12,40 @@ import enum
 import os
 
 #DATABASE_URL = os.getenv('DATABASE_URL')
-TIME = datetime.time(hour=0, minute=0, tzinfo=zoneinfo.ZoneInfo('Asia/Tokyo'))
+ZONE_TOKYO = zoneinfo.ZoneInfo('Asia/Tokyo')
+TIME = datetime.time(hour=16, minute=51, tzinfo=ZONE_TOKYO)
 
 
 class Progress(base.Command):
     def __init__(self, bot: discord.ext.commands.Bot):
         super().__init__(bot=bot)
         self.printer.start()
+        print(self.printer.next_iteration)
+        self.channel: Optional[discord.TextChannel] = bot.get_channel(1099254013347758100)
 
     @commands.command()
     async def progress(self, ctx: commands.Context):
-        pass
+        if self.printer.is_running() == False:
+            self.printer.start()
+        self.channel = ctx.channel
+        await ctx.channel.send('このチャンネルを進捗報告チャンネルとして登録しました。')
 
     @tasks.loop(time=TIME)
     async def printer(self):
-        pass
+        members_not_sent = self.channel.members
+        now_time = datetime.datetime.now(tz=ZONE_TOKYO)
+        start_time = now_time - datetime.timedelta(days=1)
+        async for message in self.channel.history(after=start_time):
+            if message.author in members_not_sent:
+                members_not_sent.remove(message.author)
+
+        if len(members_not_sent) > 0:
+            mentions = ''
+            for member in members_not_sent:
+                mentions = '{0} {1}'.format(mentions, member.mention)
+            await self.channel.send(embed=discord.Embed(
+                title='今日の進捗はどうですか？', description=mentions
+            ))
 
 
 async def setup(bot: discord.ext.commands.Bot):
